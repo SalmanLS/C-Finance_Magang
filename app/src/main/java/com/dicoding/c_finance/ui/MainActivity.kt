0bc347b82820384f6173b4f19f0f7bc1f7d61731
SalmanLS <salmanluthfi456@gmail.com>
2024-12-22 03:19:19 +0700

    Creating UI for Home, Users, Cashflow, Log

--- app/src/main/java/com/dicoding/c_finance/ui/MainActivity.kt
+++ app/src/main/java/com/dicoding/c_finance/ui/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.dicoding.c_finance.ui
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.c_finance.R
 import com.dicoding.c_finance.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,37 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        enableEdgeToEdge()
+
+        val bottomNavigationView = binding.bnView
+
+        supportFragmentManager.beginTransaction().replace(R.id.flFragment, HomeFragment())
+            .commit()
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.flFragment, HomeFragment())
+                        .commit()
+                    true
+                }
+                R.id.nav_users -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.flFragment, UsersFragment())
+                        .commit()
+                    true
+                }
+                R.id.nav_list -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.flFragment, CashflowFragment())
+                        .commit()
+                    true
+                }
+                R.id.nav_log -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.flFragment, LogFragment())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
